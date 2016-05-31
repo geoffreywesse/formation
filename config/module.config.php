4616631f32aea4116ff3a82f13217893f1af052c
geoffreywesse <wessege08@mairie.fr>
2016-05-31 16:14:49 +0200

    Formation - 05310216 - 16h14
    Création de blocs tels que liste de vidéos ; carrousel vidéo ; géoloc
    avec champ KML

--- config/module.config.php
+++ config/module.config.php
@@ -10,10 +10,25 @@ return array(
                     '/css/bootstrap.min.css',
                 ),
                 'js' => array(
+                    "/js/blockDefinitions.js"
                 ),
                 'img' => array(
                 )
             ),
         )
     ),
+    'blocksDefinition' => array(
+        'carouselVideo' => array(
+            'maxlifeTime' => 60,
+            'definitionFile' => realpath(__DIR__ . "/blocks/") . '/carouselVideo.json'
+        ),
+        'contentVideoList' => array(
+            'maxlifeTime' => 60,
+            'definitionFile' => realpath(__DIR__ . "/blocks/") . '/contentVideoList.json'
+        ),
+        'geoSearchResults' => array(
+            'maxlifeTime' => 60,
+            'definitionFile' => realpath(__DIR__ . "/blocks/")  . '/geoSearchResults.json'
+        ),
+    )
 );
---
+++ themes/formation/js/blockDefinitions.js
@@ -0,0 +1,10 @@
+blocksConfig.carouselVideo={
+    "template": "/templates/blocks/carouselVideo.html",
+    "internalDependencies":["/src/modules/rubedoBlocks/controllers/CarouselVideoController.js"],
+    "externalDependencies":['/components/OwlFonk/OwlCarousel/owl-carousel/owl.carousel.min.js']
+};
+
+blocksConfig.contentVideoList={
+    "template": "/templates/blocks/contentVideoList.html",
+    "internalDependencies":["/src/modules/rubedoBlocks/controllers/contentVideoListController.js","/src/modules/rubedoBlocks/directives/PaginatorDirective.js"]
+};
